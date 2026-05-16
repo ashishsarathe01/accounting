7bc2c2eafa20468659da0ef27911d9c6a3bd5e5b
Khushi <khushi17kuhar@gmail.com>
2026-05-16 10:46:31 +0530

    m

--- routes/web.php
+++ routes/web.php
@@ -107,15 +107,7 @@ use App\Http\Controllers\AdminModuleController\GSTComplianceController;
 use App\Http\Controllers\DutiesCompliance\DutiesComplianceController;
 use App\Http\Controllers\AdminModuleController\DefaultPrivilegesController;
 use App\Http\Controllers\ReceiptRegister\ReceiptRegisterController;
-use App\Http\Controllers\PaymentRegister\PaymentRegisterReportController;
-<<<<<<< Updated upstream
-use App\Http\Controllers\tdsReport\TdsReportController;
-use App\Http\Controllers\BoxController;
-=======
-use App\Http\Controllers\production\OpeningStockReelWiseController;
-use App\Http\Controllers\MerchantCredentials\MerchantCredentialsController;
->>>>>>> Stashed changes
-/*
+use App\Http\Controllers\PaymentRegister\PaymentRegisterReportController;/*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
@@ -1315,26 +1307,8 @@ Route::get('/journal/print/{id}', [JournalController::class, 'print'])->name('jo
     Route::get('/report/hsn/detail', [gstR1Controller::class, 'hsnDetail']);  
     
     //TDS Report working
-<<<<<<< Updated upstream
-
-    Route::get('/tds-194c-report', [TdsReportController::class, 'tds194c'])->name('tds.194c.report');
-    Route::get('/tds-194c-report/view', [TdsReportController::class, 'tds194cView'])->name('tds.194c.report.view');
-    Route::get('/receipt-register', [ReceiptRegisterController::class, 'index'])->name('receipt_register');
-    Route::get('/payment-register',[PaymentRegisterReportController::class, 'index'])->name('payment_register');
-    Route::get('/box-calculator', [BoxController::class, 'index']);
-    Route::get('/box-calculator/advance', [BoxController::class, 'advanceindex'])
-    ->name('box-calculator.advance');
-    Route::get('/gstr1/summary', [gstR1Controller::class, 'getReturnSummary'])
-    ->name('gstr1.summary');
-=======
      Route::get('/tds-194c-report', [TdsReportController::class, 'tds194c'])->name('tds.194c.report');
      Route::get('/tds-194c-report/view', [TdsReportController::class, 'tds194cView'])->name('tds.194c.report.view');
 Route::get('/receipt-register', [ReceiptRegisterController::class, 'index'])->name('receipt_register');
 Route::get('/payment-register',[PaymentRegisterReportController::class, 'index'])->name('payment_register');
-Route::get('payment-register-modal-details',[PaymentRegisterReportController::class, 'paymentRegisterModalDetails'])->name('payment.register.modal.details');
-Route::get('receipt-register-modal-details',[ReceiptRegisterController::class, 'receiptRegisterModalDetails'])->name('receipt.register.modal.details');
-Route::get('/reel-ledger/modal-detail', [OpeningStockReelWiseController::class, 'modalDetail'])->name('reel.ledger.modal.detail');
-Route::get('merchant-credentials',[MerchantCredentialsController::class, 'index'])->name('merchant_credentials.index');
-Route::post('merchant-credentials/store',[MerchantCredentialsController::class, 'store'])->name('merchant_credentials.store');
->>>>>>> Stashed changes
 });
